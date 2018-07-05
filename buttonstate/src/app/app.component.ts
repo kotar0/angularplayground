2a3b8079ca9f21c3def59877a7bb67a8cda156b0
kotar0 <koutaro.okuya@gmail.com>
2018-07-05 20:48:21 +0900

    途中

--- buttonstate/src/app/app.component.ts
+++ buttonstate/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/timer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  state = 'initial';
+  stream$ = Observable.of(0);
+  subject$ = new BehaviorSubject<any>(0);
+
+  ngOnInit() {
+    this.subject$.subscribe(this.stream$);
+  }
+
+  start() {
+    this.stream$.subscribe(succ => {
+      this.state = 'prossesing';
+      Observable.timer(1000).subscribe(succ => {
+        this.state = 'complete';
+      });
+    });
+  }
 }
